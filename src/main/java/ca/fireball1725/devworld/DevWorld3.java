40567089cf024f785683be6622220069741d9c3c
FireBall1725 <fireball@fireball1725.com>
2022-08-31 23:36:10 -0400

    new buttons

--- src/main/java/ca/fireball1725/devworld/DevWorld3.java
+++ src/main/java/ca/fireball1725/devworld/DevWorld3.java
@@ -1,9 +1,12 @@
 package ca.fireball1725.devworld;
 
+import ca.fireball1725.devworld.client.events.DevWorldClientEvents;
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -21,6 +24,7 @@ public class DevWorld3 {
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> DevWorldClientEvents::new);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
