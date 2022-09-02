e505f2c16ecde7fc87008df010ac80b2cc022f4b
FireBall1725 <fireball@fireball1725.com>
2022-09-02 15:36:53 -0400

    wip

--- src/main/java/ca/fireball1725/devworld/client/events/DevWorldClientEvents.java
+++ src/main/java/ca/fireball1725/devworld/client/events/DevWorldClientEvents.java
@@ -1,11 +1,17 @@
 package ca.fireball1725.devworld.client.events;
 
+import ca.fireball1725.devworld.util.DevWorldUtils;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.PlainTextButton;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.WorldStem;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import org.apache.logging.log4j.core.pattern.PlainTextRenderer;
 import org.slf4j.Logger;
 
 public class DevWorldClientEvents {
@@ -17,30 +23,56 @@ public class DevWorldClientEvents {
     private int keyShiftCount = 0;
 
     public DevWorldClientEvents() {
+        MinecraftForge.EVENT_BUS.addListener(this::keyPress);
         MinecraftForge.EVENT_BUS.addListener(this::renderMainMenu);
         MinecraftForge.EVENT_BUS.addListener(this::initMainMenu);
     }
 
-    public void renderMainMenu(ScreenEvent.Render.Post event) {
+    public void keyPress(ScreenEvent.KeyPressed.Post event) {
         if (event.getScreen() instanceof TitleScreen titleScreen) {
-            buttonCreate.visible = true;
+            LOGGER.info(">>> Key Press: " + event.getKeyCode());
+
+            if (event.getKeyCode() == 340)
+                keyShiftCount ++;
+
+            if (keyShiftCount >= 2)
+                buttonDelete.active = true;
         }
     }
 
-    public void initMainMenu(ScreenEvent.Init.Post event) {
+    public void renderMainMenu(ScreenEvent.Render.Post event) {
         if (event.getScreen() instanceof TitleScreen titleScreen) {
-            int buttonY = event.getScreen().height / 4 + 48;
+            buttonDelete.visible = true;
+            buttonCreate.visible = true;
+
+            //GuiComponent.drawCenteredString();
+        }
+    }
+
+    public void initMainMenu(ScreenEvent.Init event) {
+        if (event.getScreen() instanceof TitleScreen titleScreen) {
+            int buttonY = event.getScreen().height / 4 + 38;
             int buttonX = event.getScreen().width / 2 + 104;
 
+            PlainTextButton devWorldText = new PlainTextButton(buttonX, buttonY, 84, 20, Component.literal("DevWorld 3"), button -> {}, Minecraft.getInstance().font);
+
+
+            buttonY += 10;
             buttonCreate = new Button(buttonX, buttonY, 84, 20, Component.translatable("devworld.menu.new"), button -> {
-                LOGGER.info(">>> Create new devworld");
-                titleScreen.renderables.clear();
+                try {
+                    DevWorldUtils devWorldUtils = new DevWorldUtils();
+                    devWorldUtils.createDevWorld();
+                } catch (Exception e) {
+                    LOGGER.error(e.getMessage());
+                }
             });
-            buttonLoad = new Button(buttonX, buttonY, 40, 20, Component.translatable("devworld.menu.load"), button -> {
+
+            buttonLoad = new Button(buttonX, buttonY, 84, 20, Component.translatable("devworld.menu.load"), button -> {
                 LOGGER.info(">>> Load devworld");
             });
-            buttonX += 44;
-            buttonDelete = new Button(buttonX, buttonY, 40, 20, Component.translatable("devworld.menu.delete"), button -> {
+
+            buttonY += 24;
+            buttonDelete = new Button(buttonX, buttonY, 84, 20, Component.translatable("devworld.menu.delete"), button -> {
                 LOGGER.info(">>> Delete devworld");
                 keyShiftCount = 0;
             });
@@ -52,9 +84,10 @@ public class DevWorldClientEvents {
 
             keyShiftCount = 0;
 
-            titleScreen.renderables.add(buttonCreate);
-            titleScreen.renderables.add(buttonLoad);
-            titleScreen.renderables.add(buttonDelete);
+            event.addListener(devWorldText);
+            event.addListener(buttonCreate);
+            event.addListener(buttonLoad);
+            event.addListener(buttonDelete);
         }
     }
 }
