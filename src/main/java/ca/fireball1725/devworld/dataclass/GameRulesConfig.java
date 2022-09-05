2855b0c3b5417e555e8356fc6abdc48c7c2dd4fa
FireBall1725 <fireball@fireball1725.com>
2022-09-05 14:35:55 -0400

    final touches on the PR

--- src/main/java/ca/fireball1725/devworld/dataclass/GameRulesConfig.java
+++ src/main/java/ca/fireball1725/devworld/dataclass/GameRulesConfig.java
@@ -3,12 +3,27 @@ package ca.fireball1725.devworld.dataclass;
 import com.google.gson.annotations.SerializedName;
 
 public class GameRulesConfig {
-    @SerializedName("daylight_cycle")
-    public Boolean daylightCycle = false;
+    @SerializedName("rule_daylight")
+    public Boolean ruleDayLightCycle = false;
 
     @SerializedName("daylight_time")
     public Integer daylightTime = 6000;
 
-    @SerializedName("weather_cycle")
-    public Boolean weatherCycle = false;
+    @SerializedName("rule_weather_cycle")
+    public Boolean ruleWeatherCycle = false;
+
+    @SerializedName("rule_dofiretick")
+    public Boolean ruleDoFireTick = false;
+
+    @SerializedName("rule_mobgriefing")
+    public Boolean ruleMobGriefing = false;
+
+    @SerializedName("rule_domobspawning")
+    public Boolean ruleDoMobSpawning = true;
+
+    @SerializedName("rule_disable_raids")
+    public boolean ruleDisableRaids = true;
+
+    @SerializedName("rule_do_insomnia")
+    public Boolean ruleDoInsomnia = false;
 }
